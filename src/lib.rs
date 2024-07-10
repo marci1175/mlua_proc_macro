93aa430061555f78e10225ae666ee234f1800225
Varga Marcell <128537619+marci1175@users.noreply.github.com>
2024-07-10 16:44:13 +0200

    yes

--- src/lib.rs
+++ src/lib.rs
@@ -16,9 +16,13 @@ fn convert_to_table_impl(input: ItemStruct) -> proc_macro::TokenStream {
     for field in &fields.named {
         //If there was the skip attr present we continue, so that we dont generate code for this field
         for attr in &field.attrs {
-            if attr.path().is_ident("table") {
+            //We can manually skip an entry or if there is a serde skip ettribute that means that it cant be turned into a lua table entry anyway
+            if attr.path().is_ident("table") || attr.path().is_ident("serde") {
+
+                //This is set to true by the closure if we want to skip this entry
                 let mut should_skip = false;
 
+                //Check if there is a skip attribute in the table or serde attribute
                 attr.parse_nested_meta(|meta| {
                     if meta.path.is_ident("skip") {
                         should_skip = true;
@@ -28,24 +32,32 @@ fn convert_to_table_impl(input: ItemStruct) -> proc_macro::TokenStream {
                 })
                 .unwrap();
 
+                //If we should skip this entry
                 if should_skip {
                     continue;
                 }
             }
         }
 
+        //Name of the entry
         let ident = field.ident.as_ref().unwrap();
+        
+        //Name of the entry as string
         let string = ident.to_string();
+
+        //Statement, this is the code representation of the automaticly added line
         let statement = quote! {
             table.set(#string, serde_json::to_string(&self.#ident).unwrap());
         };
 
+        //Back up all the statements
         statements.push(statement);
     }
 
     let name = &input.ident;
     let (impl_generics, ty_generics, where_clause) = input.generics.split_for_impl();
 
+    //Create function
     quote! {
         impl #impl_generics #name #ty_generics #where_clause {
             fn set_table_from_struct(&self, lua: &mlua::Lua) {
--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,9 @@ struct Application {
     frame_count: i64,
 
     #[table(skip)]
-    test: Info,
+    info: Info,
+
+    field1: String,
 }
 
 #[derive(serde::Serialize, serde::Deserialize, Default)]
@@ -18,7 +20,8 @@ impl Application {
     pub fn new() -> Self {
         Self {
             frame_count: 0,
-            test: Info::default(),
+            info: Info::default(),
+            field1: String::from("Hello world!")
         }
     }
 
@@ -26,7 +29,10 @@ impl Application {
         loop {
             self.set_table_from_struct(lua);
 
-            // lua.load(r#"print(vars.test)"#).exec()?;
+            lua.load(r#"
+            print(string.format("Frame count: %i", vars.frame_count))
+            print(string.format("Field 1: %s", vars.field1))
+            "#).exec()?;
 
             self.frame_count += 1;
         }
